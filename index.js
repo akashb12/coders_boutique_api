0579e33d15b9f40feb07989eb39ec0911a1ff4b0
akashbanaulikar <akashbanaulikar05@gmail.com>
2024-09-21 01:16:41 +0530

    trying cors issue fix

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin:["https://coders-boutique-front.vercel.app/"],
-    credentials:true
-}));
+const corsOptions = {
+    origin: ['https://coders-boutique-front.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
 app.use('/api',routes)
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
